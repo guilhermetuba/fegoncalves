695850a0500a498366d5c32e7fd8dec9f6e4449c
guilhermetuba <guilhermetuba@gmail.com>
2025-03-09 23:35:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const URL_API = "https://script.google.com/macros/s/AKfycbzXiY4-7QLvVpr95yy7h13x8bXPhh44pny3QlNUD_xa9fjNYLFWCpm8JzU3lnUgvC2b/exec";
+const URL_API = "https://script.google.com/macros/s/AKfycbyIcU1hrF_uC1-qjsKqjBz_6aQJ9OOVYg-tOWTpuA9pafpwYiPr9GD7Ui4c1MtCZ35s/exec";
 console.log("Config.js carregado! URL_API:", URL_API);
